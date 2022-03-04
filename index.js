0c421a2a911e7c567f2c24ebfe26e1862a450a01
acfier <acfier@gmail.com>
2022-03-03 23:46:06 -0500

    commit tree 7fc609dc06d0f438d4ef1ecd97299cc23f97ccce

---
+++ index.js
@@ -0,0 +1,34 @@
+// function shout(string) {
+//     // todo
+// }
+
+// function shout(string) {
+//     return string;
+// }
+
+// "Hello!".toUpperCase ();
+
+function shout(string) {
+    return string.toUpperCase ();
+}
+// function whisper(string) {
+//     return string;
+// }
+
+// "hello!".toLowerCase ();
+
+function whisper(string) {
+    return string.toLowerCase ();
+}
+
+function logShout(string) {
+    console.log(shout(string))
+}
+
+function logWhisper(string) {
+    console.log(whisper(string));
+}
+function sayHiToHeadphonedRoommate(string) {
+    if(string === string.toLowerCase()) {
+        return "I can't hear you!"}}
+
